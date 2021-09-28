cb02315d5df724b52feb276d706bed02bd2cc325
sandraottersten <otterstensandra@gmail.com>
2021-09-28 13:04:06 +0200

    Storybook added

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,12 +1,15 @@
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
-const cssnano = require("cssnano");
+const postcssimport = require("postcss-import");
+const nesting = require("postcss-nesting");
 
 const mode = process.env.NODE_ENV;
 const dev = mode === "development";
 
 const config = {
 	plugins: [
+        postcssimport(),
+        nesting(),
         //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
         tailwindcss(),
         //But others, like autoprefixer, need to run after,
