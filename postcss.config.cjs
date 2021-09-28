4a7d5af6a53c826e00526274849e8516f58b63ef
nilskj <nils@queenslab.se>
2021-09-28 23:55:53 +0200

    - Fix tailwind setup in storybook

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,23 +1,8 @@
-const tailwindcss = require("tailwindcss");
-const autoprefixer = require("autoprefixer");
-const postcssimport = require("postcss-import");
-const nesting = require("postcss-nesting");
-
-const mode = process.env.NODE_ENV;
-const dev = mode === "development";
-
-const config = {
-	plugins: [
-        postcssimport(),
-        nesting(),
-        //Some plugins, like tailwindcss/nesting, need to run before Tailwind,
-        tailwindcss(),
-        //But others, like autoprefixer, need to run after,
-        autoprefixer(),
-        !dev && cssnano({
-			preset: "default",
-		})
-    ],
+module.exports = {
+	plugins: {
+		'postcss-import': {},
+		'postcss-nesting': {},
+		autoprefixer: {},
+		tailwindcss: {}
+	}
 };
-
-module.exports = config;
